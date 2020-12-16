7230249d2a8326b6285e2eaca966d414a4816c17
wujinfeng953 <739890112@qq.com>
2020-12-16 22:17:05 +0800

    表单验证完成

--- src/script/cart_module.js
+++ src/script/cart_module.js
@@ -34,10 +34,46 @@ define(['jcookie'], () => {
                             $clonebox.find('.b-sum strong').html((value.price * num).toFixed(2));
                             $clonebox.css('display', 'block'); //显示
                             $('.item-list').append($clonebox); //追加
+                            calcprice(); //计算总价
                         }
                     });
                 });
             }
+            //计算总价和总的商品件数
+            function calcprice() {
+                let $sum = 0; //商品的件数
+                let $count = 0; //商品的总价
+                $('.goods-item:visible').each(function(index, ele) {
+                    if ($(ele).find('.cart-checkbox input').prop('checked')) { //复选框是否勾选
+                        $sum += parseInt($(ele).find('.quantity-form input').val());
+                        $count += parseFloat($(ele).find('.b-sum strong').html());
+                    }
+                });
+                //赋值
+                $('.amount-sum').find('em').html($sum);
+                $('.totalprice').html($count.toFixed(2));
+            }
+
+            // 4.全选 - 事件委托。
+
+            $('.allsel').on('click', function() {
+                $('.goods-item:visible').find(':checkbox').prop('checked', $(this).prop('checked')); //将全选的值给下面的几个input
+                $('.allsel').prop('checked', $(this).prop('checked')); //将自己的值赋值给自己。2个allsel复选框
+                calcprice(); //计算总价
+            });
+
+            //获取克隆的商品列表里面的checkbox,添加事件
+            //克隆的商品列表里面：选中的复选框的长度等于存在的复选框的长度
+            let $inputs = $('.goods-item:visible').find(':checkbox'); //查找复选框
+            $('.item-list').on('click', $inputs, function() {
+                //$(this):被委托的元素，checkbox
+                if ($('.goods-item:visible').find(':checkbox').length === $('.goods-item:visible').find('input:checked').size()) {
+                    $('.allsel').prop('checked', true);
+                } else {
+                    $('.allsel').prop('checked', false);
+                }
+                calcprice(); //计算总价
+            });
 
 
 
--- src/script/registry_module.js
+++ src/script/registry_module.js
@@ -2,9 +2,21 @@ define([], () => {
     return {
         init: function() {
             const $username = $('#username');
+            const $password = $('#password');
             const $span = $('.main-form-span');
+            const $border = $('.main-form-input');
+            const $from = $('from');
+            let $userflag = null;
+            let $pwdflag = null;
+
+
+            $username.on('focus', function() {
+                $span.eq(0).css('color', 'green').html('中英文均可，最长14个英文或7个汉字');
+                $border.eq(0).css('border', '1px solid green')
+            })
 
             $username.on('blur', function() {
+                let $_this = $(this);
                 $.ajax({
                     type: 'post',
                     url: 'http://10.31.161.50/dashboard/360mall-porject/php/reg.php',
@@ -12,13 +24,122 @@ define([], () => {
                         xingming: $username.val()
                     }
                 }).done(function(data) { //data就是后端返回的结果
+                    console.log(data);
                     if (!data) { //不存在
-                        $span.css('color', 'green').html('√');
+                        if ($_this.val() !== '') {
+                            let $strleng = $_this.val().replace(/[\u4e00-\u9fa5]/g, '**').length;
+
+                            if ($strleng > 0 && $strleng <= 14) {
+
+                                let $reg = /^[a-zA-Z\u4e00-\u9fa5]+$/;
+                                if ($reg.test($_this.val())) {
+                                    $span.eq(0).css('color', 'green').html('√');
+                                    $border.eq(0).css('border', '1px solid #999')
+                                    $userflag = true;
+
+                                }
+                            } else {
+                                $span.eq(0).css('color', 'red').html('请输入正确的长度')
+                                $border.eq(0).css('border', '1px solid red')
+                                $userflag = false;
+                            }
+                        } else {
+                            $span.eq(0).css('color', 'red').html('用户名不能为空');
+                            $border.eq(0).css('border', '1px solid red')
+                            $userflag = false;
+
+                        }
                     } else { //存在
-                        $span.css('color', 'red').html('该用户名已存在');
+                        $span.eq(0).css('color', 'red').html('该用户名已存在');
+                        $border.eq(0).css('border', '1px solid red')
+                        $userflag = false;
+
+
                     }
                 });
+
+
             });
+            // 密码得到焦点
+            $password.on('focus', function() {
+                $span.eq(1).css('color', 'green').html('请输入密码');
+                $border.eq(1).css('border', '1px solid green')
+            });
+            $password.on('input', function() {
+                let $_this = $(this).val();
+                if ($_this.length > 7 && $_this.length < 20) {
+                    let $regnum = /\d+/;
+                    let $regUpperCase = /[A-Z]+/;
+                    let $regLowerCase = /[a-z]+/;
+                    let $other = /[\W_]+/;
+                    let $num = 0;
+                    if ($regnum.test($_this)) {
+                        $num++;
+                    }
+                    if ($regLowerCase.test($_this)) {
+                        $num++;
+                    }
+                    if ($regUpperCase.test($_this)) {
+                        $num++;
+                    }
+                    if ($other.test($_this)) {
+                        $num++;
+                    }
+                    switch ($num) {
+                        case 1:
+                            $span.eq(1).css('color', 'red').html('弱');
+                            $pwdflag = false;
+                            break;
+                        case 2:
+                        case 3:
+                            $span.eq(1).css('color', 'blue').html('中');
+                            $pwdflag = true;
+                            break;
+                        case 4:
+                            $span.eq(1).css('color', 'green').html('强');
+                            $pwdflag = true;
+                            break;
+
+                    }
+                } else {
+                    $span.eq(1).css('color', 'red').html('请输入8-20位的密码');
+                    $pwdflag = false;
+                }
+            })
+
+
+
+            // 密码框失去焦点
+            $password.on('blur', function() {
+
+                let $_this = $(this).val();
+                if ($_this !== '') {
+                    if ($pwdflag) {
+                        $span.eq(1).css('color', 'green').html('√');
+                        $pwdflag = true;
+                    }
+                } else {
+                    $span.eq(1).css('color', 'red').html('密码不能为空')
+                    $border.eq(1).css('border', '1px solid red')
+                    $pwdflag = false;
+                }
+            })
+
+            $from.on('submit', function() {
+                if ($username.val() === '') {
+                    $span.eq(0).css('color', 'red').html('用户名不能为空');
+                    $userflag = false;
+                }
+                if ($password.val() === '') {
+                    $span.eq(1).css('color', 'red').html('密码不能为空')
+                    $pwdflag = false;
+                }
+                if (!$userflag || !$pwdflag) {
+                    return false;
+                }
+            })
+
+
 
         }
     }
